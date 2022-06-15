d5bdc9cf20f7842147d62f67b3b2b9ffdcdf59d0
jviitsm <jvictorgmodesto@gmail.com>
2022-06-15 14:50:33 -0300

    novaTransferencia component, ngfor implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alura-bytebank';
+  transferencias: any[] = [];
+
+  transfer($event){
+    this.transferencias.push({...$event, data: new Date()});
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-nova-transferencia",
@@ -7,12 +7,22 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent {
 
+    @Output() onTransfer = new EventEmitter<any>();
+
     valor: number;
     destino: number;
 
     transferir(){
-      console.log(
-       this.valor, this.destino
-      )
+        this.onTransfer.emit({
+            valor: this.valor,
+            destino: this.destino
+        });
+
+        this.limparCampos();
+    }
+
+    limparCampos(){
+        this.valor = 0;
+        this.destino = 0;
     }
 }
